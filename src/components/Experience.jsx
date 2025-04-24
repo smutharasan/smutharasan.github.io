e044a59fc409045a8257e09af2b56fafd92638c4
Supriya Mutharasan <59520945+smutharasan@users.noreply.github.com>
2025-04-24 03:28:07 -0400

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -51,7 +51,6 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        {" "}
         <p className={styles.sectionSubText}>My Journey So Far</p>
         <h2 className={styles.sectionHeadText}>Work Experience</h2>
       </motion.div>
