a72e7d3c2568825a6f79b8ea4e28b888bcb72662
Supriya Mutharasan <59520945+smutharasan@users.noreply.github.com>
2025-04-24 01:53:45 -0400

    Update index.js with good resume points

--- src/constants/index.js
+++ src/constants/index.js
@@ -125,10 +125,15 @@ const experiences = [
     iconBg: '#E6DEDD',
     date: 'Jan 2023 - Apr 2024',
     points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
+      'Enhancing transparency and accountability by implementing a centralized tracking system using PowerApps and Power Automate, reducing unresolved issues by 25%.',
+      'Enabling intake and project coordinator teams to track BR progress electronically through an automated checklist and exception logic, reducing manual effort and communication delays.',
+      'Driving a 30% increase in system adoption by creating over 10 user guides, infographics, and organizational visuals to clarify system benefits and support stakeholder integration.',
+      'Resolving WCAG compliance issues by fixing tab order and enhancing data visibility for screen readers, improving platform inclusivity for assistive technology users.',
+      'Standardizing the intake process using a Power Automate flow to sort and categorize over 250 monthly business request emails, reducing workload and improving assignment accuracy.',
+      'Analyzing historical BR data using Excel functions like VLOOKUP to identify milestone trends and provide actionable insights for improved planning and process optimization.',
+      'Designing and integrating over 10 UML class diagrams into the business architecture document to simplify system understanding and facilitate collaboration across teams.',
+      'Developing a bilingual, searchable Power BI glossary of over 200 terms to streamline onboarding, enhance team alignment, and reduce dependency on live instruction.',
+      'Producing 20 bilingual software training videos to onboard executives and Linux/Unix teams, increasing efficiency and reducing miscommunication through consistent resources.'
     ],
   },
   {
@@ -138,10 +143,11 @@ const experiences = [
     iconBg: '#383E56',
     date: 'May 2021 - Dec 2021',
     points: [
-      'Developing and maintaining web applications using React.js and other related technologies.',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
+      'Enhancing AODA compliance of the public-facing website by using tools like SiteImprove and custom HTML scripts to scan and improve accessibility across the board’s digital presence.',
+      'Improving user navigation on the sports event web application by introducing filters for event type and gender, enhancing the overall user experience.',
+      'Remediating and quality-testing over 100 web pages, 50 school websites, and 20 modules under strict timelines to ensure WCAG-compliant accessibility.',
+      'Collaborating with over 20 school principals and marketing officers to design and update school and public webpages, improving design consistency and content clarity.',
+      'Deploying and testing the staff login page used board-wide under a tight deadline, delivering essential access functionality reliably and on time.'
     ],
   },
   {
